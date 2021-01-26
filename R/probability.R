b9fccd4b3b942380aa431c25d5a43609f551e8d1
vgherard <vgherard@sissa.it>
2021-01-26 22:11:12 +0100

    Added 'Add-k', 'Laplace' and 'ML' methods

--- R/probability.R
+++ R/probability.R
@@ -1,12 +1,20 @@
 #' @export
-probability <- function(freqs, word, context, method = c("StupidBackoff"), par)
+probability <- function(freqs, word, context, 
+                        method = c("StupidBackoff", "Add-k", "Laplace", "ML"), 
+                        par
+                        )
 {
+        check_method(method, par)
+        f <- attr(freqs, "cpp_obj")
         if (method == "StupidBackoff") {
-                if (missing(par) || is.null(par[["lambda"]])) {
-                        stop("Stupid Backoff requires parameter 'lambda'")
-                }
-                lambda <- par[["lambda"]]
-                probability_sbo(attr(freqs, "cpp_obj"), word, context, lambda)        
+                res <- probability_sbo(f, word, context, par[["lambda"]])        
+        } else if (method == "Add-k") {
+                res <- probability_addk(f, word, context, par[["k"]])        
+        } else if (method == "Laplace") {
+                res <- probability_addk(f, word, context, 1.0)        
+        } else if (method == "ML") {
+                res <- probability_ml(f, word, context)        
         }
         
+        return(prob)
 }
