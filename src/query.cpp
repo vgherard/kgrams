c48a8fa3c0593800db0d9a56dd3d3b8a5884d113
vgherard <vgherard@sissa.it>
2021-01-26 14:38:43 +0100

    Split computation of kgram_code in separate function

--- src/query.cpp
+++ src/query.cpp
@@ -1,21 +1,25 @@
 #include "kgramFreqs.h"
+#include <utility>
+
+std::pair<size_t, std::string> kgramFreqs::kgram_code (std::string kgram) const
+{
+        std::pair<size_t, std::string> res{0, ""};
+        WordStream stream(kgram);
+        std::string word, index;
+        for (; ; res.first++) {
+                word = stream.pop_word();
+                if (stream.eos()) break;
+                index = dict_.index(word);
+                res.second += index + " ";
+        }
+        if (res.first > 0) 
+                res.second.pop_back();
+        return res;
+}
 
 double kgramFreqs::query (std::string kgram) const {
-        WordStream stream(kgram);
-        std::string kgram_code = "", word, index;
-        size_t k = 0;
-        for (; ; k++) {
-                word = stream.pop_word();
-                if (stream.eos())
-                        break;
-                index = dict_.index(word);
-                kgram_code += index + " ";
-        }
-        
-        if (k > 0) 
-                kgram_code.pop_back();
-        if (k > N_)
-                return -1; // NA
-        auto it = freqs_[k].find(kgram_code);
-        return it != freqs_[k].end() ? it->second : 0;
+        auto p = kgram_code(kgram);
+        if (p.first > N_) return 0;
+        auto it = freqs_[p.first].find(p.second);
+        return it != freqs_[p.first].end() ? it->second : 0;
 }
