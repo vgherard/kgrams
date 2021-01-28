03e882365823e61fff0cb0b99575b724416b6649
vgherard <vgherard@sissa.it>
2021-01-28 21:29:39 +0100

    Temporary export S4 class kgramFreqs and Dictionary for debug

--- R/RcppModules.R
+++ R/RcppModules.R
@@ -1,4 +1,6 @@
+#' @export kgramFreqs
 Rcpp::loadModule(module = "kgramFreqs", TRUE)
 Rcpp::loadModule(module = "Probability", TRUE)
 Rcpp::loadModule(module = "Utilities", TRUE)
+#' @export Dictionary
 Rcpp::loadModule(module = "Dictionary", TRUE)
