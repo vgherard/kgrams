d12b574619c1094bb60c8f7746b8541c545dd2e5
vgherard <vgherard@sissa.it>
2021-01-28 16:30:25 +0100

    Remove old comment

--- src/CircularBuffer.h
+++ src/CircularBuffer.h
@@ -7,7 +7,6 @@ class CircularBuffer {
         size_t pos_;
         std::vector<T> stream_;
 public:
-        // Constructor, initializing first size_ - 1 positions to 'init'. 
         CircularBuffer (size_t size, T init) 
                 : size_(size), pos_(0), stream_(size, init) {}
         void lshift () { pos_ = (pos_ + 1) % size_; }
