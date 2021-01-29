58847e5ca55db6c9c87449ddafdf918e60a47021
vgherard <vgherard@sissa.it>
2021-01-29 18:26:12 +0100

    Remove export tags for modules

--- R/RcppModules.R
+++ R/RcppModules.R
@@ -1,6 +1,4 @@
-#' @export kgramFreqs
 Rcpp::loadModule(module = "kgramFreqs", TRUE)
+Rcpp::loadModule(module = "Dictionary", TRUE)
 Rcpp::loadModule(module = "Probability", TRUE)
 Rcpp::loadModule(module = "Utilities", TRUE)
-#' @export Dictionary
-Rcpp::loadModule(module = "Dictionary", TRUE)
