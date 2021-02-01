2919b92331c506e11f045c7cbf1709de21a8cb00
vgherard <vgherard@sissa.it>
2021-02-01 10:56:26 +0100

    Conditional infix operator

---
+++ R/conditional_op.R
@@ -0,0 +1,20 @@
+#' @export
+`%|%` <- function(word, context) {
+        if (!is.character(word)) {
+                msg <- "lhs of %|% must be a character vector."
+                rlang::abort(message = msg, class = "infix_domain_error") 
+        }
+        if (!is.character(context) || length(context) != 1) {
+                msg <- "rhs of %|% must be a length one character vector."
+                rlang::abort(message = msg, class = "infix_domain_error") 
+        }
+        structure(list(word = word, context = context), 
+                  class = "kgrams_word_context")
+}
+
+#' @export
+print.kgrams_word_context <- function(x, n = 5, ...) {
+        cat("Word-context conditional expression: ")
+        cat(paste0(head(x$word, n = n), "|", x$context, collapse = ", "))
+        if (length(x$word) > n) cat(", ...")
+}
