4f2dc263ba89346ccb3752c6b58aaa778a8c8e6b
vgherard <vgherard@sissa.it>
2021-02-01 10:54:06 +0100

    String concatenation infix

---
+++ R/string_concatenation.R
@@ -0,0 +1,18 @@
+#' String concatenation
+#' 
+#' Brief synthax for string concatenation.
+#' 
+#' @param lhs a string or vector of strings.
+#' @param rhs a string or vector of strings.
+#' @return a string or vector of strings.
+#' @details The expression \code{lhs %+% rhs} is equivalent to
+#' \code{paste(lhs, rhs, sep = " ", collapse = NULL, recycle0 = FALSE)}.
+#' See \link[base]{paste} for more details.
+#' @examples 
+#' "i love" %+% c("cats", "jazz", "you")
+#' @seealso \link[base]{paste}
+#' 
+#' @export
+`%+%` <- function(lhs, rhs) {
+        return(paste(lhs, rhs, sep = " ", collapse = NULL, recycle0 = FALSE))
+}
