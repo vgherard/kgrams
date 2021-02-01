f1b18774a3e80e1993396c79128d7bb4d6f58b52
vgherard <vgherard@sissa.it>
2021-02-01 10:52:47 +0100

    Add unary operator() to generic smoother for sentence probabilities

--- src/Smoothing.h
+++ src/Smoothing.h
@@ -8,6 +8,7 @@
 #include "kgramFreqs.h"
 #include <cmath>
 #include <limits>
+#include <stdexcept>
 
 /// @class Smoother
 /// @brief Backbone structure for other smoothers object considered below. 
@@ -17,12 +18,22 @@ protected:
         size_t N_; ///< @brief order of k-gram model
         size_t V_; ///< @brief Size of dictionary
         
+        /// @brief Begin-Of-Sentence padding
+        const std::string padding_;
+        //--------Private methods--------//
+        
+        /// @brief Initialize a buffer of prefixes for processing sentences
+        /// N.B. this is not the same as for kgramFreqs, as Smoother work in 
+        /// decoding (i.e. they use actual word tokens)
+        /// Moreover, this includes only the top-level (N-1)-gram prefix
+        std::string generate_padding();
+        
         /// @brief truncate 'context' to last N - 1 words.
         std::string truncate (std::string context) const; // Smoothing.cpp
 public:
         /// @brief constructor
         Smoother (const kgramFreqs & f) 
-                : f_(f), N_(f.N()), V_(f.V()) {}
+                : f_(f), N_(f.N()), V_(f.V()), padding_(generate_padding()) {}
         
         /// @brief model order getter
         size_t N () const { return N_; }
@@ -44,6 +55,9 @@ public:
         // method.
         virtual double operator() (const std::string &, std::string) const
                 { return 1. ;}
+        
+        /// @brief get smoothed sentence probabilites. 
+        double operator() (const std::string &) const;
 };
 
 /// @class SBOSmoother
@@ -51,7 +65,6 @@ public:
 class SBOSmoother : public Smoother {
         //--------Private variables--------//
         double lambda_; ///< @brief Backoff penalization
-        // ToDo: define getter and setter and export as property!
         
         //--------Private methods--------//
         
@@ -71,7 +84,16 @@ public:
         SBOSmoother (const kgramFreqs & f, const double lambda) 
                 : Smoother(f), lambda_(lambda) {}
         
-        
+        //--------Parameters getters/setters--------//
+        double lambda() const { return lambda_; }
+        void set_lambda(double lambda) {
+                if (lambda < 0 or lambda > 1)
+                        throw std::domain_error(
+                                "'lambda' must be between 0 and 1."
+                                );
+                lambda_ = lambda;
+        }
+                
         //--------Probabilities--------//
         
         // Compute SBO continuation scores. Defined in Smoothing.cpp
@@ -83,7 +105,6 @@ public:
 class AddkSmoother : public Smoother {
         //--------Private variables--------//
         double k_; ///< @brief constant weight added to k-gram counts
-        // ToDo: define getter and setter and export as property!
 public:
         //--------Constructor--------//
         
@@ -94,6 +115,16 @@ public:
         /// @param k positive number. Constant weight added to k-gram counts.
         AddkSmoother (const kgramFreqs & f, const double k) : Smoother(f), k_(k) 
         {}
+        
+        //--------Parameters getters/setters--------//
+        double k() const { return k_; }
+        void set_k(double k) {
+                if (k <= 0)
+                        throw std::domain_error(
+                                        "'k' must be positive."
+                        );
+                k_ = k;
+        }
         
         //--------Probabilities--------//
 
@@ -127,7 +158,6 @@ class KNSmoother : public Smoother {
 
         //--------Private variables--------//
         double D_; ///< @brief Discount 
-        // ToDo: define getter and setter and export as property!
         
         /// @brief Left continuation counts for Kneser-Ney smoothing
         std::vector<FrequencyTable> l_continuations_;
@@ -151,7 +181,17 @@ class KNSmoother : public Smoother {
 public:
         //--------Constructors--------//
         KNSmoother (const kgramFreqs & f, const double D); // Smoothing.cpp
-
+        
+        //--------Parameters getters/setters--------//
+        double discount() const { return D_; }
+        void set_discount(double D) {
+                if (D < 0 or D > 1)
+                        throw std::domain_error(
+                                        "Discount must be between 0 and 1."
+                        );
+                D_ = D;
+        }
+        
         //--------Probabilities--------//
         // KN probabilities. Defined in Smoothing.cpp
         double operator() (const std::string & word, std::string context) const;
